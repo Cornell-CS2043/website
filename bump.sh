ab7e0bb6f5ca6fda72d67c0e372a1f6b31b22ff3
Matthew Milano <milano@cs.cornell.edu>
2019-01-24 23:04:23 -0500

    adding generate site stuff

--- bump.sh
+++ bump.sh
@@ -1,2 +1,14 @@
-git add _site && git commit -m 'bump site version'
-git push
+origin=`pwd`
+if [ -f .gitmodules ]; then 
+	cd _site
+	git checkout LICENSE
+	git add *
+	git commit -a -m 'adding generate site stuff'
+	git push
+	cd $origin
+	git add _data/lecture-source/
+	git add _site && git commit -m 'bump site/lecture version'
+	git push
+else
+	echo "need to be in git root to run this"
+fi
